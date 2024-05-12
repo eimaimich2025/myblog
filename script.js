664145f037749d031671bef5717443c08dd4b6ce
eimaimich2025 <164413208+eimaimich2025@users.noreply.github.com>
2024-05-11 22:11:47 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+function toggleBanner() {
+    var banner = document.getElementById("mySideBanner");
+    banner.style.width = banner.style.width === '250px' ? '0' : '250px';
+}
+
+// Close the side banner
+function closeBanner() {
+    var banner = document.getElementById("mySideBanner");
+    banner.style.width = '0';
+}
